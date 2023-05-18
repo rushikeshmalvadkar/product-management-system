8b40f45cdf3c36ccdafba139394e3e97d916d07a
Ruhikesh Malvadkar <rushikeshmalvadkar@gmail.com>
2023-05-18 19:15:50 +0530

    add junit for getPageRequestList method of productservice

--- pms/src/test/java/in/test/pms/service/ProductServiceTest.java
+++ pms/src/test/java/in/test/pms/service/ProductServiceTest.java
@@ -1,16 +1,26 @@
 package in.test.pms.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.failBecauseExceptionWasNotThrown;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import in.test.pms.dto.request.CreateProductRequest;
+import in.test.pms.dto.request.ProductPageRequest;
+import in.test.pms.dto.response.ProductPagedResponse;
 import in.test.pms.dto.response.ProductResponse;
 import in.test.pms.entity.ProductEntity;
 import in.test.pms.exception.ProductNotFoundException;
@@ -68,6 +78,35 @@ class ProductServiceTest {
 		ProductResponse productResponce = productService.fetchProductById(1);
 		assertThat(productResponce.getProductId()).isEqualTo(1);
 	}
+	
+	@Test
+	void getPagedProductList() {
+		ProductRepository productRepoMock = Mockito.mock(ProductRepository.class);
+		ProductMapper productMapper = new ProductMapper();
+		ProductEntity productEntity = new ProductEntity();
+		productEntity.setId(1);
+		productEntity.setName("mobile");
+		List<ProductEntity> products = new ArrayList<>();
+          products.add(productEntity);
+          
+        ProductPageRequest productPageRequest = new ProductPageRequest();
+        productPageRequest.setPageNumber(1);
+        productPageRequest.setPageSize(1);
+		Integer pageNumber=productPageRequest.getPageNumber();
+		Integer pageSize=productPageRequest.getPageSize();
+		//Pageable pageble = PageRequest.of(pageNumber,pageSize);
+		Pageable pageable = PageRequest.of(pageNumber,pageSize);
+		
+		ProductEntity productEntityExampale = new ProductEntity();
+		productEntity.setName("mobile");
+		Example<ProductEntity> exampale = Example.of(productEntityExampale);
+		Page page = new PageImpl<>(products, pageable, 1);
+		
+		Mockito.when(productRepoMock.findAll(Mockito.any(Example.class) ,Mockito.any(Pageable.class))).thenReturn(page);
+		ProductService productService = new ProductService(productMapper, productRepoMock);
+		ProductPagedResponse pagedProductList = productService.getPagedProductList(productPageRequest);
+		assertEquals(1, pagedProductList.getProductList().size());
+	} 
 	
 	
 	
